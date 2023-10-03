9caf31838608109389268a8d7e57cf31f0ca1ab4
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-03 15:04:26 +0100

    whats app

---
+++ learningAxis/whatsApp/index.js
@@ -0,0 +1,24 @@
+
+function sendToWhatsapp() {
+  let number = "+237673461722";
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  var url =
+    "https://wa.me/" +
+    number +
+    "?text=" +
+    "Name : " +
+    name +
+    "%0a" +
+    "Email : " +
+    email +
+    "%0a" +
+    "Message : " +
+    message +
+    "%0a%0a";
+
+  window.open(url, "_blank").focus();
+}
